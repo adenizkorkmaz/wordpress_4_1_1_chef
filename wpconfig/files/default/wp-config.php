bf06a7424ef70f9807af9b7a40bf27d09ce8baba
adenizkorkmaz <adenizkorkmaz@gmail.com>
2016-02-11 14:42:33 +0200

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -14,8 +14,8 @@
  * @package WordPress
  */
 define('WP_REDIS_HOST', 'wordpress.uc1k7r.0001.usw2.cache.amazonaws.com');
-define('WP_HOME','http://d2sgx0tdqa0sak.cloudfront.net');
-define('WP_SITEURL','http://d2sgx0tdqa0sak.cloudfront.net');
+define('WP_HOME','http://d1pqdleys5m0i8.cloudfront.net');
+define('WP_SITEURL','http://d1pqdleys5m0i8.cloudfront.net');
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'wordpress');
